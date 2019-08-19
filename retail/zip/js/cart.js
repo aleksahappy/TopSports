641e1d520842d0e04c0c2fec010512ea850e74e4
Aleksahappy <aleksa.happy@gmail.com>
2019-08-19 10:55:14 +0400

    Добавлен проект розничного сайта по экипировке. Доработаны фильтры.

---
+++ retail/zip/js/cart.js
@@ -0,0 +1,151 @@
+'use strict';
+
+//=====================================================================================================
+//  Функции добавления товаров в корзину:
+//=====================================================================================================
+
+// Удаление значения из инпута при его фокусе:
+
+function onFocusInput(input) {
+  if (input.value != '') {
+    input.value = '';
+  }
+}
+
+// Установка дефолтного значения в инпут:
+
+function onBlurInput(input) {
+  if (input.value == '') {
+    input.value = 0;
+  }
+  changeValue(event);
+}
+
+// Запрет на ввод в инпут любого значения кроме цифр:
+
+function checkValue(event) {
+  if (event.ctrlKey || event.altKey || event.metaKey) return;
+  var chr = getChar(event);
+  if (chr == null) return;
+  if (chr < '0' || chr > '9') {
+    return false;
+  }
+}
+
+// Кросс-браузерная функция для получения символа из события keypress:
+
+function getChar(event) {
+  if (event.which == null) { // IE
+    if (event.keyCode < 32) return null; // спец. символ
+    return String.fromCharCode(event.keyCode)
+  }
+  if (event.which != 0 && event.charCode != 0) { // все кроме IE
+    if (event.which < 32) return null; // спец. символ
+    return String.fromCharCode(event.which); // остальные
+  }
+  return null; // спец. символ
+}
+
+
+// Изменение информации о выбранном количестве товара и его стоимости:
+
+function changeValue(event) {
+  var card = event.currentTarget.closest('.card'),
+      size = event.currentTarget.closest('.card-size'),
+      articul = size.querySelector('.size-articul').textContent.replace('Артикул: ', ''),
+      input = size.querySelector('.choice-gty'),
+      inputValue = parseInt(input.value),
+      qty = size.querySelector('.card-qty'),
+      qtyValue = parseInt(qty.textContent),
+      curPrice = input.dataset.price;
+
+  var totalAmount = getInfo('cartInfo') ? getInfo('cartInfo').totalAmount : 0;
+  var totalPrice = getInfo('cartInfo') ? getInfo('cartInfo').totalPrice : 0;
+
+  if (event.currentTarget.value === undefined) {
+    var sign = event.currentTarget.textContent;
+    if (sign == '-') {
+      if (inputValue > 0 ) {
+        inputValue--;
+      }
+    }
+    if (sign == '+') {
+      if (inputValue < qtyValue) {
+        inputValue++;
+      }
+    }
+  } else {
+    if (isNaN(inputValue)) {
+      inputValue = 0;
+    }
+    if (inputValue > qtyValue) {
+      inputValue = qtyValue;
+    }
+  }
+  input.value = inputValue;
+
+  var change = inputValue - parseInt(input.dataset.value);
+  totalAmount = totalAmount + change;
+  totalPrice = totalPrice + change * curPrice;
+
+  input.dataset.value = inputValue;
+  changeColors(size, inputValue);
+  saveSizesInfo(articul, inputValue);
+  changeCardInfo(card);
+  saveCartInfo(totalAmount, totalPrice);
+  changeCartInfo();
+}
+
+// Изменение цвета элементов карточки при измении количества выбранных товаров:
+
+function changeColors(size, inputValue) {
+  if (inputValue == 0) {
+    size.classList.remove('in-cart');
+  } else {
+    size.classList.add('in-cart');
+  }
+}
+
+// Сохранение данных о выбранных размерах:
+
+function saveSizesInfo(key, value) {
+  var sizesInfo = getInfo('sizesInfo') ? getInfo('sizesInfo') : {};
+  if (value == 0) {
+    delete sizesInfo[key];
+  } else {
+    sizesInfo[key] = value;
+  }
+  saveInfo('sizesInfo', sizesInfo);
+}
+
+// Сохранение данных о состоянии корзины:
+
+function saveCartInfo(totalAmount, totalPrice) {
+  var cartInfo = getInfo('cartInfo')? getInfo('cartInfo') : {};
+  cartInfo.totalAmount = totalAmount;
+  cartInfo.totalPrice = totalPrice;
+  saveInfo('cartInfo', cartInfo);
+}
+
+// Изменение информации о выбранном количестве товара и его стоимости в карточке товара:
+
+function changeCardInfo(card) {
+  var selectInfo = card.querySelector('.card-select-info'),
+      sizes = card.querySelectorAll('.choice-gty'),
+      curPrice = sizes[0].dataset.price,
+      amount = card.querySelector('.select-count'),
+      price = card.querySelector('.select-price');
+
+  var totalAmount = 0;
+  sizes.forEach(size => totalAmount += parseInt(size.value));
+  var totalPrice = totalAmount * curPrice;
+
+  amount.textContent = totalAmount;
+  price.textContent = convertPrice(totalPrice);
+
+  if (amount.textContent != 0) {
+    selectInfo.style.visibility = 'visible';
+  } else {
+    selectInfo.style.visibility = 'hidden';
+  }
+}
